faa6673c9019906d1ffe8142fab9cc28bdb705f6
FayezRizqi <rizkipro122@gmail.com>
2023-04-17 21:57:43 +0700

    tambahan f/button

--- mintask/minitask.ino
+++ mintask/minitask.ino
@@ -5,7 +5,12 @@ pinMode (13,HIGH);
 }
 
 void loop(){
-
+  if (digitalRead(buttonPin) == HIGH) {  
+    digitalWrite(ledPin, HIGH);  
+    Serial.println("High - Fayez ");
+  } else {  
+    digitalWrite(ledPin, LOW);  
+    Serial.println("Low - Fayez ");
 
 
 }
